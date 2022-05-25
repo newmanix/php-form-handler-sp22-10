372fa3c4104e68296925290e97f263999a9f0f34
Bill Newman <william.newman@seattlecolleges.edu>
2022-05-25 11:23:57 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
     <title>PHP Test Page</title>
   </head>
   <body>
-    <h1>PHP Test Page, Can you see this?</h1>
+    <h1>PHP Test Page</h1>
     <?php
     echo '<p>This is PHP!</p>';
     ?>
-  
+    <p>Hello from GitHub!</p>
   </body>
 </html>
